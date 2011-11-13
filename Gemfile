5cf2b7434fee128cc139e382732b9415635734cb
kana <nicht@s8.xrea.com>
2011-11-13 14:45:10 +0900

    Add skeleton of route to show HTML :help

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source :rubygems
 
 gem 'haml'
+gem 'httpclient'
 gem 'sinatra'
--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require 'cgi'
 require 'haml'
+require 'httpclient'
 require 'sinatra'
 
 
@@ -11,6 +12,41 @@ class App < Sinatra::Application
 
   get '/' do
     haml :index
+  end
+
+  get '/:host/:user/:repos/:ref/*.html' do |host, user, repos, ref, path|
+    case host
+    when 'github'
+      response = fetch_github(user, repos, ref, path)
+    else
+      halt 403, "Host '#{host}' is not supported."
+    end
+
+    # FIXME: Tweak caching.
+    if response.status == 200 then
+      haml :help,
+           locals: {
+             html_help: VimHelp.htmlize(response.body),
+           }
+    else
+      halt response.status, response.header['Status']
+    end
+  end
+
+  helpers do
+    def fetch(uri)
+      HTTPClient.new.get(uri)
+    end
+
+    def fetch_github(user, repos, ref, path)
+      fetch("https://raw.github.com/#{user}/#{repos}/#{ref}/#{path}")
+    end
+  end
+end
+
+class VimHelp
+  def self.htmlize(s)
+    s  # FIXME: Implement.
   end
 end
 
